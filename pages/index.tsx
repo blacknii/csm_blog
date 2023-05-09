46678bc93762f5864ba3ea8c9d400ef573202bbd
Dominik Stanczyk <stanczykdominik0@gmail.com>
2023-05-10 00:43:48 +0200

    .

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,41 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import { PostCard, Categories, PostWidget } from '../components'
+import type { NextPage } from "next";
+import Head from "next/head";
+import { PostCard, Categories, PostWidget } from "../components";
+import { getPosts } from "../services";
 
-const posts = [
-  {title : "React Testing", excerpt: "Learn React Testing"},
-  {title : "React with Tailwind", excerpt: "Learn React with Tailwind"}
-]
+// const posts = [
+//   { title: "React Testing", excerpt: "Learn React Testing" },
+//   { title: "React with Tailwind", excerpt: "Learn React with Tailwind" },
+// ];
 
-const Home: NextPage = () => {
+export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
         <title>CMS Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
-        <div className='lg:col-span-8 col-span-1'>
-        {posts.map((post) => <PostCard post={post} key={post.title} />
-        )}
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post) => (
+            <PostCard post={post} key={post.title} />
+          ))}
         </div>
-      <div className='lg:col-span-4 col-span-1'>
-        <div className='lg:sticky relative top-8'>
-          <PostWidget />
-          <Categories />
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relative top-8">
+            <PostWidget />
+            <Categories />
+          </div>
         </div>
-
-      </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export async function getStaticPropts() {
+  const posts = (await getPosts()) || [];
+
+  return {
+    props: { posts },
+  };
+}
