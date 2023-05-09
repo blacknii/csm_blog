efd7489fa71902296cc5e5a9bd67cf88fe834b78
Dominik Stanczyk <stanczykdominik0@gmail.com>
2023-05-10 00:44:03 +0200

    .

--- services/index.js
+++ services/index.js
@@ -1,9 +1,47 @@
 import { request, gql } from "graphql-request";
 
-export const getPost = () => {
-  const querty = gql`
-    query MyQuery {
+const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
 
+export const getPost = async () => {
+  const query = gql`
+    query Assets {
+      assets {
+        createdAt
+        id
+        publishedAt
+        fileName
+        url
+        updatedAt
+      }
+      postsConnection {
+        edges {
+          node {
+            author {
+              bio
+              name
+              id
+              photo {
+                url
+              }
+            }
+            createdAt
+            slug
+            title
+            excerpt
+            featuredImage {
+              url
+            }
+            categories {
+              name
+              slug
+            }
+          }
+        }
+      }
     }
   `;
+
+  const result = await request(graphqlAPI, query);
+
+  return result.postConnection.edges;
 };
